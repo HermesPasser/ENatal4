cd671df3098397a3f170233a8ac5bb4d77b8a3cb
Dr.XGB <dr.xgb.rm2k@gmail.com>
2021-12-14 20:53:54 -0300

    Desafio3 lançado

---
+++ main.c
@@ -0,0 +1,35 @@
+/*
+ * =================================================================
+ * Desafio de Natal
+ *
+ * Nível: Intermediário
+ *
+ * Objetivo:
+ * Ler o CSV com a lista de todas as crianças contendo seu nome,
+ * idade e o presente pedido para este Natal.
+ * Após isso, o sistema deve organizar a lista com a idade
+ * das crianças, começando da mais nova até a mais velha.
+ * =================================================================
+ */
+
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(int argv, char** argc)
+{
+	setlocale(LC_ALL, "portuguese");
+
+	puts("*** LOTE DE PRESENTES - #211213 ***");
+	puts("-----------------------------------\n");
+	puts("Lista de Pedidos:\n");
+
+	/*
+	 * ====================================
+	 * Seu código começa aqui
+	 * ====================================
+	 */
+
+	puts("\n-----------------------------------\n");
+	return 0;
+}
